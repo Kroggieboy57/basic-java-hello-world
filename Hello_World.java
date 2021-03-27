da2e90034e4df6677fac3d69d0dcdb40dc512e99
Krogs <66489697+Kroggieboy57@users.noreply.github.com>
2021-03-27 20:01:06 +0530

    Java file of the code

--- Hello_World.java
+++ Hello_World.java
@@ -2,12 +2,12 @@
 /**
  * This Program on execution simply prints the statement Hello World
  * @author Kroggieboy57
- * @version 1
+ * @version 1.1
  */
 public class Hello_World
 {
     public static void main(String args [])
     {
-        System.out.println("Hello World");//This statement simply prints whatever is enclosed within the "" into the console
+        System.out.println("Hello World");//This statement simply prints whatever is enclosed within the "" into the terminal
     }
 }
